0bc5a572eca81c99684dcc957049685e33e3e0e1
JocelynGV <jv06042@georgiasouthern.edu>
2024-04-13 18:19:22 -0400

    pushing current updates that allow limited functionality

--- functions.js
+++ functions.js
@@ -1,6 +1,10 @@
+const piece = ["X", "O"]
+
 function displayBoard() {
     // get board div from html file
     var board = document.getElementById("board");
+    board.style.visibility = "visible";
+    board.replaceChildren();
 
     for(var i = 1; i <=9; i++) {
         // create grid elements and add to grid div
@@ -12,9 +16,18 @@ function displayBoard() {
     }
 }
 
-displayBoard();
+// display board after pressing start
+var start = document.getElementById("start");
+start.addEventListener("click", displayBoard);
+start.addEventListener("mouseover", event => {
+    event.target.style.backgroundColor = "lavender";
+});
+start.addEventListener("mouseout", event => {
+    event.target.style.backgroundColor = "beige";
+});
+
 
 function addPiece(event) {
-//     var curr = document.getElementById(id);
-    // curr.innerHTML = "x";
+    // event.target.style.backgroundColor = "red";
+    event.target.innerHTML ="X";
 }
