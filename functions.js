3104e46324ac2836e25b398bc6089251892a8d04
mk13151 <mk13151@georgiasouthern.edu>
2024-04-14 23:56:46 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -86,7 +86,7 @@ function changePlayer() {
     player.innerHTML = piece[index % 2] + "'s turn!";
 }
 
-var draw = false
+var draw = false;
 var gridItems = board.childNodes;
 function checkWinner() {
     
@@ -119,7 +119,7 @@ function checkWinner() {
         winner = gridItems[2].id;
         endGame();
         return true;
-
+    }
 
     draw = true;
     for(var i = 0; i < gridItems.length; i++ ){
@@ -132,18 +132,16 @@ function checkWinner() {
         winner = "Draw";
         endGame();
         return true;
+    }
 }
 
 var playAgain = document.getElementById("playAgain");
 
-
+function endGame() {
     if(draw){
         player.innerHTML = winner ;
     }
-    player.style.
-    player.innerHTML = winner + " wins!";
-    // player.style.backgroundColor = "yellow";
-    document.getElementById("body").style
+    player.style.backgroundColor = "yellow";
     isOver = true;
 
     playAgain.style.visibility = "visible";
@@ -153,8 +151,7 @@ var playAgain = document.getElementById("playAgain");
 
 function resetBoard() {
     player.innerHTML = "";
-    player.style.backgroundColor = "rgba(0
-    document.getElementById("body").style.backgroundColor = "rgb
+    player.style.backgroundColor = "rgba(0,0,0,0)";
     playAgain.style.visibility = "hidden";
     isOver = false;
 }
